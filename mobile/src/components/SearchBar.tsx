3b3497c35a886d4a6bb48a9f45d4e0223b230ae6
David Alvarez <david@southfloridaathleticclub.com>
2022-03-21 03:29:03 -0400

    search bar component

---
+++ mobile/src/components/SearchBar.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View} from 'react-native';
+import {Text} from 'react-native-svg';
+
+const SearchBar: React.FC = () => {
+  return (
+    <View>
+      <Text>Search Bar</Text>
+    </View>
+  );
+};
+
+export default SearchBar;
