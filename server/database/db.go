4804aaaf0ff860967b81b65a73768cc48c851717
David Alvarez <david@southfloridaathleticclub.com>
2022-04-02 17:00:36 -0400

    db conn setup

--- server/database/db.go
+++ server/database/db.go
@@ -1,34 +1,37 @@
 package database
+
 import (
-	"gorm.io/gorm"
-	"gorm.io/driver/postgres"
-	"github.com/joho/godotenv"
 	"fmt"
+	"log"
 	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 var db *gorm.DB
 
 type connection struct {
-	Host string
-	Port int
-	User string
+	Host     string
+	Port     string
+	User     string
 	Password string
-	DB string
+	DB       string
 }
 
 func Connect() {
-	err := godotenv.load()
+	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading dot env file")
 	}
 
-	conn := connection {
-		Host: os.Getenv("POSTGRES_HOST")
-		Port: os.Getenv("POSTGRES_PORT")
-		User: os.Getenv("POSTGRES_USER")
-		Password: os.Getenv("POSTGRES_PASSWORD")
-		DB: os.Getenv("POSTGRES_DB")
+	conn := connection{
+		Host:     os.Getenv("POSTGRES_HOST"),
+		Port:     os.Getenv("POSTGRES_PORT"),
+		User:     os.Getenv("POSTGRES_USER"),
+		Password: os.Getenv("POSTGRES_PASSWORD"),
+		DB:       os.Getenv("POSTGRES_DB"),
 	}
 
 	db, err := gorm.Open(postgres.Open(connToString(conn)), &gorm.Config{})
@@ -37,19 +40,14 @@ func Connect() {
 		fmt.Printf("Error connecting to the DB: %s\n", err.Error())
 		return
 	} else {
-		fmt.Printf("DB is open\n")
+		fmt.Printf("Connected to Database.")
 	}
 
-	err = db.Ping()
-	if err != nil {
-		fmt.Printf("Error could not ping database: %s\n", err.Error())
-		return
-	} else {
-		fmt.Printf("DB pinged successfully\n")
-	}
+	db.AutoMigrate()
 
-	func connToString(info connection) string {
-		return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-			info.Host, info.Port, info.User, info.Password, info.DBName)
-	}
+}
+
+func connToString(info connection) string {
+	return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		info.Host, info.Port, info.User, info.Password, info.DB)
 }
